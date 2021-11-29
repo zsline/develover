8d57d1b78a0a9122461173065d1efd340013d071
zsline <sergei.dereka@gmal.com>
2021-11-29 20:55:32 +0200

    slider

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,23 @@ $(function () {
     document.body.classList.toggle("light")
     localStorage.theme = document.body.className || ""
   }
+
+
+})
+$(function () {
+  $('.services__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.services__info'
+  });
+  $('.services__info').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.services__slider',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
 })
